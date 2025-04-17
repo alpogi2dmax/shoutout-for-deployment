67f3f3a484e2805c88eea18f90871a5c7ae6fdf8
Al Cerdan Lico <alcerdan@yahoo.com>
2025-04-17 13:38:50 -0700

    add handlefollow and handle unfollow in UserPage

--- client/src/components/UserPage.js
+++ client/src/components/UserPage.js
@@ -2,7 +2,7 @@ import { useEffect, useState, useContext } from "react"
 import { useParams, Link, useNavigate, Outlet } from "react-router-dom"
 import { UserPageContext } from "../context/userpage"
 import { UserContext } from "../context/user"
-// import UserNavBar from "./UserNavBar"
+import UserNavBar from "./UserNavBar"
 
 function UserPage() {
 
@@ -29,72 +29,72 @@ function UserPage() {
         })
     }, [id])
 
-    // const handleFollowClick = () => {
-    //     let values = {
-    //         follower_id: user.id,
-    //         followed_id: userPage.id
-    //     }
-    //     fetch("/follows", {
-    //         method: "POST",
-    //         headers: {
-    //             "Content-Type": "application/json"
-    //         },
-    //         body: JSON.stringify(values, null, 2),
-    //     })
-    //     .then((r) => r.json())
-    //     .then(() => {
-    //         const newFollower = {
-    //             id: user.id,
-    //             first_name: user.first_name,
-    //             last_name: user.last_name,
-    //             profile_pic: user.profile_pic,
-    //             username: user.username
-    //         }
-    //         const newFollowed = {
-    //             id: userPage.id,
-    //             first_name: userPage.first_name,
-    //             last_name: userPage.last_name,
-    //             profile_pic: userPage.profile_pic,
-    //             username: userPage.username
-    //         }
-    //         const updatedUserPage = {
-    //             ...userPage,
-    //             followers: [...userPage.followers, newFollower]
-    //         }
-    //         const updatedFollowed = [newFollowed, ...followed]
+    const handleFollowClick = () => {
+        let values = {
+            follower_id: user.id,
+            followed_id: userPage.id
+        }
+        fetch("https://shoutout-for-deployment.onrender.com/follows", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(values, null, 2),
+        })
+        .then((r) => r.json())
+        .then(() => {
+            const newFollower = {
+                id: user.id,
+                first_name: user.first_name,
+                last_name: user.last_name,
+                profile_pic: user.profile_pic,
+                username: user.username
+            }
+            const newFollowed = {
+                id: userPage.id,
+                first_name: userPage.first_name,
+                last_name: userPage.last_name,
+                profile_pic: userPage.profile_pic,
+                username: userPage.username
+            }
+            const updatedUserPage = {
+                ...userPage,
+                followers: [...userPage.followers, newFollower]
+            }
+            const updatedFollowed = [newFollowed, ...followed]
 
-    //         updateUserPage(updatedUserPage)
-    //         setFollowed(updatedFollowed)
-    //     })
-    // }
+            updateUserPage(updatedUserPage)
+            setFollowed(updatedFollowed)
+        })
+    }
 
-    // const handleUnfollowClick = () => {
-    //     const follow = {
-    //         follower_id: user.id,
-    //         followed_id: userPage.id
-    //     }
-    //     fetch('/follow_delete', {
-    //         method: "DELETE",
-    //         headers: {
-    //             'Content-Type': 'application/json'
-    //         },
-    //         body: JSON.stringify(follow)
-    //     })
-    //     .then(() => {
-    //         const updatedFollowers = userPage.followers.filter(follower => follower.id !== user.id)
-    //         const updatedUserPage = {
-    //             ...userPage,
-    //             followers: updatedFollowers
-    //         }
-    //         const updatedFollowed = user.followed.filter(followed => followed.id !== userPage.id)
-    //         const updatedUser = {
-    //             ...user,
-    //             followed: updatedFollowed
-    //         }
-    //         updateUserPage(updatedUserPage)
-    //         setUser(updatedUser)
-    //     })
-    // }
+    const handleUnfollowClick = () => {
+        const follow = {
+            follower_id: user.id,
+            followed_id: userPage.id
+        }
+        fetch('https://shoutout-for-deployment.onrender.com/follow_delete', {
+            method: "DELETE",
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(follow)
+        })
+        .then(() => {
+            const updatedFollowers = userPage.followers.filter(follower => follower.id !== user.id)
+            const updatedUserPage = {
+                ...userPage,
+                followers: updatedFollowers
+            }
+            const updatedFollowed = user.followed.filter(followed => followed.id !== userPage.id)
+            const updatedUser = {
+                ...user,
+                followed: updatedFollowed
+            }
+            updateUserPage(updatedUserPage)
+            setUser(updatedUser)
+        })
+    }
 
     if(!userPage) return  <div>Loading...</div>
 
